7150c1b6a35a3c509cc9af0a1cd091451cab2566
KatherineRincon <ingry_krincon@soy.sena.edu.co>
2025-04-10 17:39:46 -0500

    api de base

---
+++ controllers/artistas.go
@@ -0,0 +1,83 @@
+package controllers
+
+import (
+	"fmt"
+
+	"github.com/astaxie/beego"
+)
+
+// ArtistasController operations for Artistas
+type ArtistasController struct {
+	beego.Controller
+}
+
+// URLMapping ...
+func (c *ArtistasController) URLMapping() {
+	c.Mapping("Post", c.Post)
+	c.Mapping("GetOne", c.GetOne)
+	c.Mapping("GetAll", c.GetAll)
+	c.Mapping("Put", c.Put)
+	c.Mapping("Delete", c.Delete)
+}
+
+// Post ...
+// @Title Create
+// @Description create Artistas
+// @Param	body		body 	models.Artistas	true		"body for Artistas content"
+// @Success 201 {object} models.Artistas
+// @Failure 403 body is empty
+// @router / [post]
+func (c *ArtistasController) Post() {
+
+}
+
+// GetOne ...
+// @Title GetOne
+// @Description get Artistas by id
+// @Param	id		path 	string	true		"The key for staticblock"
+// @Success 200 {object} models.Artistas
+// @Failure 403 :id is empty
+// @router /:id [get]
+func (c *ArtistasController) GetOne() {
+
+}
+
+// GetAll ...
+// @Title GetAll
+// @Description get Artistas
+// @Param	query	query	string	false	"Filter. e.g. col1:v1,col2:v2 ..."
+// @Param	fields	query	string	false	"Fields returned. e.g. col1,col2 ..."
+// @Param	sortby	query	string	false	"Sorted-by fields. e.g. col1,col2 ..."
+// @Param	order	query	string	false	"Order corresponding to each sortby field, if single value, apply to all sortby fields. e.g. desc,asc ..."
+// @Param	limit	query	string	false	"Limit the size of result set. Must be an integer"
+// @Param	offset	query	string	false	"Start position of result set. Must be an integer"
+// @Success 200 {object} models.Artistas
+// @Failure 403
+// @router / [get]
+func (c *ArtistasController) GetAll() {
+	fmt.Println("get all artistas")
+
+}
+
+// Put ...
+// @Title Put
+// @Description update the Artistas
+// @Param	id		path 	string	true		"The id you want to update"
+// @Param	body		body 	models.Artistas	true		"body for Artistas content"
+// @Success 200 {object} models.Artistas
+// @Failure 403 :id is not int
+// @router /:id [put]
+func (c *ArtistasController) Put() {
+
+}
+
+// Delete ...
+// @Title Delete
+// @Description delete the Artistas
+// @Param	id		path 	string	true		"The id you want to delete"
+// @Success 200 {string} delete success!
+// @Failure 403 id is empty
+// @router /:id [delete]
+func (c *ArtistasController) Delete() {
+
+}
